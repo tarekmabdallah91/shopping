c999e64f920e71e1030836f803d2b0fa0092fa21
tarekmabdallah91 <tarekmabdallah91@gmail.com>
2022-09-15 18:06:13 +0300

    - save values and submit a form

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:shopping/providers/product.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const route = '/EditProductScreen';
@@ -11,11 +14,25 @@ class _EditProductScreenState extends State<EditProductScreen> {
   final _descriptionFocusNode = FocusNode();
   final _imageUrlController = TextEditingController();
   final _imageUrlFocusNode = FocusNode();
+  final _form = GlobalKey<FormState>();
+  var editedProduct = Product(
+    id: '',
+    title: '',
+    description: '',
+    price: 0,
+    imageUrl: '',
+  );
 
   @override
   void initState() {
     _imageUrlFocusNode.addListener(_updateImageUrl);
     super.initState();
+  }
+
+  void _saveForm() {
+    _form.currentState!.save();
+    print(
+        '${editedProduct.id}, ${editedProduct.title}, ${editedProduct.description}, ${editedProduct.price}, ${editedProduct.imageUrl}');
   }
 
   @override
@@ -39,10 +56,18 @@ class _EditProductScreenState extends State<EditProductScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Edit Product'),
+        actions: [
+          IconButton(
+              onPressed: _saveForm,
+              icon: Icon(
+                Icons.save,
+              ))
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
+          key: _form,
           child: ListView(
             children: <Widget>[
               TextFormField(
@@ -51,18 +76,45 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
                 },
+                onSaved: (value) {
+                  editedProduct = Product(
+                    id: editedProduct.id,
+                    title: value!,
+                    description: editedProduct.description,
+                    price: editedProduct.price,
+                    imageUrl: editedProduct.imageUrl,
+                  );
+                },
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Price'),
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.number,
                 focusNode: _priceFocusNode,
+                onSaved: (value) {
+                  editedProduct = Product(
+                    id: editedProduct.id,
+                    title: editedProduct.title,
+                    description: editedProduct.description,
+                    price: double.parse(value!),
+                    imageUrl: editedProduct.imageUrl,
+                  );
+                },
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Description'),
                 maxLines: 3,
                 keyboardType: TextInputType.multiline,
                 focusNode: _descriptionFocusNode,
+                onSaved: (value) {
+                  editedProduct = Product(
+                    id: editedProduct.id,
+                    title: editedProduct.title,
+                    description: value!,
+                    price: editedProduct.price,
+                    imageUrl: editedProduct.imageUrl,
+                  );
+                },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -100,6 +152,18 @@ class _EditProductScreenState extends State<EditProductScreen> {
                         // we force Flutter to update the screen, hence picking up the latest image value entered by the user.
                         // It's not clear whether that's a bug or feature - the above code snippet will fix it though.
                         setState(() {});
+                      },
+                      onSaved: (value) {
+                        editedProduct = Product(
+                          id: editedProduct.id,
+                          title: editedProduct.title,
+                          description: editedProduct.description,
+                          price: editedProduct.price,
+                          imageUrl: value!,
+                        );
+                      },
+                      onFieldSubmitted: (_) {
+                        _saveForm();
                       },
                     ),
                   ),
