78383d5e7c93ba86dbf25639db1eefa16803f6e3
tarekmabdallah91 <tarekmabdallah91@gmail.com>
2022-09-15 13:22:56 +0300

    -add Snackbar

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -50,7 +50,21 @@ class ProductItem extends StatelessWidget {
                 color: Theme.of(context).colorScheme.secondary,
                 onPressed: () {
                   cart.addItem(product);
-                  cart.items.forEach((key, cartItem) {});
+                  Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    'Added item to cart!',
+                  ),
+                  duration: Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
                 },
               ),
             ),
