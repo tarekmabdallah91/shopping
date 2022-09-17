5f1eac560041ceedf67890c4ea2b34e30a4d836b
tarekmabdallah91 <tarekmabdallah91@gmail.com>
2022-09-17 19:18:39 +0300

    - use json for the serialization

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -2,14 +2,24 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:json_annotation/json_annotation.dart';
+import 'package:json_serializable/json_serializable.dart';
+part 'product.g.dart';
 
+// for Nested classes use annotation @JsonSerializable(explicitToJson: true)
+@JsonSerializable()
 class Product with ChangeNotifier {
   // 6th step 'with ChangeNotifier'
   String id;
+  @JsonKey(required: true)
   final String title;
+  @JsonKey(required: true)
   final String description;
+  @JsonKey(defaultValue: 0)
   final double price;
+  @JsonKey(required: true)
   final String imageUrl;
+  @JsonKey(defaultValue: false)
   bool isFavorite;
 
   Product({
@@ -20,6 +30,15 @@ class Product with ChangeNotifier {
     required this.imageUrl,
     this.isFavorite = false,
   });
+
+  /// A necessary factory constructor for creating a new User instance
+  /// The constructor is named after the source class, in this flutter pub run build_runner build --delete-conflicting-outputscase
+  factory Product.fromJson(Map<String, dynamic> json) =>
+      _$ProductFromJson(json);
+
+  /// `toJson` is the convention for a class to declare support for serialization
+  /// to JSON. The implementation simply calls the private, generated
+  Map<String, dynamic> toJson() => _$ProductToJson(this);
 
   // void toggleFavoriteStatus() {
   //   isFavorite = !isFavorite;
---
+++ lib/providers/product.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Product _$ProductFromJson(Map<String, dynamic> json) {
+  $checkKeys(json, requiredKeys: const ['title', 'description', 'imageUrl']);
+  return Product(
+    id: json['id'] as String,
+    title: json['title'] as String,
+    description: json['description'] as String,
+    price: (json['price'] as num)?.toDouble() ?? 0,
+    imageUrl: json['imageUrl'] as String,
+    isFavorite: json['isFavorite'] as bool ?? false,
+  );
+}
+
+Map<String, dynamic> _$ProductToJson(Product instance) => <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'description': instance.description,
+      'price': instance.price,
+      'imageUrl': instance.imageUrl,
+      'isFavorite': instance.isFavorite,
+    };
